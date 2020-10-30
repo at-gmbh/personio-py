f7953b41881d8000ff0d6a1d11c3a2ad180b6680
Philip Flohr <philip.flohr@cetitec.com>
2020-10-30 14:54:53 +0100

    Fix: pytest: Only query for test users is credentials are available and authentication successful

--- tests/apitest_shared.py
+++ tests/apitest_shared.py
@@ -8,22 +8,25 @@ CLIENT_ID = os.getenv('CLIENT_ID')
 CLIENT_SECRET = os.getenv('CLIENT_SECRET')
 personio = Personio(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
 
+shared_test_data = {}
+
 # deactivate all tests that rely on a specific personio instance
 try:
     personio.authenticate()
     can_authenticate = True
+    # This is used to ensure the test check for existing objects
+    test_employee = personio.get_employees()[0]
+    shared_test_data = {
+        'test_employee': {
+            'id': test_employee.id_,
+            'first_name': test_employee.first_name,
+            'last_name': test_employee.last_name,
+            'email': test_employee.email,
+            'hire_date': test_employee.hire_date
+        }
+    }
 except PersonioError:
     can_authenticate = False
 skip_if_no_auth = pytest.mark.skipif(not can_authenticate, reason="Personio authentication failed")
 
-# This is used to ensure the test check for existing objects
-test_employee = personio.get_employees()[0]
-shared_test_data = {
-    'test_employee': {
-        'id': test_employee.id_,
-        'first_name': test_employee.first_name,
-        'last_name': test_employee.last_name,
-        'email': test_employee.email,
-        'hire_date': test_employee.hire_date
-    }
-}
+
