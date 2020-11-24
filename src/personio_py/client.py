f8907a8dbd3fe521df3ec5e854bc83a70fd92345
Philip Flohr <philip.flohr@hadiko.de>
2020-11-24 19:58:25 +0100

    Raise NotImplementedError if a function is called which is not implemented (#10)

--- src/personio_py/client.py
+++ src/personio_py/client.py
@@ -268,8 +268,7 @@ class Personio:
         """
         placeholder; not ready to be used
         """
-        # TODO implement
-        pass
+        raise NotImplementedError()
 
     def get_attendances(self, employees: Union[int, List[int], Employee, List[Employee]],
                         start_date: datetime = None, end_date: datetime = None) -> List[Attendance]:
@@ -296,22 +295,19 @@ class Personio:
         """
         # attendances can be created individually, but here you can push a huge bunch of items
         # in a single request, which can be significantly faster
-        # TODO implement
-        pass
+        raise NotImplementedError()
 
     def update_attendance(self, attendance_id: int):
         """
         placeholder; not ready to be used
         """
-        # TODO implement
-        pass
+        raise NotImplementedError()
 
     def delete_attendance(self, attendance_id: int):
         """
         placeholder; not ready to be used
         """
-        # TODO implement
-        pass
+        raise NotImplementedError()
 
     def get_absence_types(self) -> List[AbsenceType]:
         """
@@ -349,22 +345,19 @@ class Personio:
         """
         placeholder; not ready to be used
         """
-        # TODO implement
-        pass
+        raise NotImplementedError()
 
     def create_absence(self, absence: Absence):
         """
         placeholder; not ready to be used
         """
-        # TODO implement
-        pass
+        raise NotImplementedError()
 
     def delete_absence(self, absence_id: int):
         """
         placeholder; not ready to be used
         """
-        # TODO implement
-        pass
+        raise NotImplementedError()
 
     def _get_employee_metadata(
             self, path: str, resource_cls: Type[PersonioResourceType],
