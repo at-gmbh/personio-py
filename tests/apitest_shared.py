c03e1d1e12c830e707b1485b74fb7e04ab478c4f
Philip Flohr <philip.flohr@cetitec.com>
2020-12-03 17:40:19 +0100

    Place authentication in importable file for use with test cases placed in multiple files (#11)
    * Place authentication in importable file for use with test cases placed in multiple files
    
    * Readd existing tests
    
    * Update import statement
    
    * Don't use shared_test_data dict, cache retrieval of valid online user
    
    * Don't require python3.8 to run the tests
    
    * cleanup
    
    Co-authored-by: Sebastian Straub <sebastian.straub@alexanderthamm.com>

--- tests/apitest_shared.py
+++ tests/apitest_shared.py
@@ -1,4 +1,7 @@
 import os
+
+from functools import lru_cache
+
 import pytest
 
 from personio_py import Personio, PersonioError
@@ -8,25 +11,15 @@ CLIENT_ID = os.getenv('CLIENT_ID')
 CLIENT_SECRET = os.getenv('CLIENT_SECRET')
 personio = Personio(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
 
-shared_test_data = {}
 
 # deactivate all tests that rely on a specific personio instance
 try:
     personio.authenticate()
     can_authenticate = True
-    # This is used to ensure the test check for existing objects
-    test_employee = personio.get_employees()[0]
-    shared_test_data = {
-        'test_employee': {
-            'id': test_employee.id_,
-            'first_name': test_employee.first_name,
-            'last_name': test_employee.last_name,
-            'email': test_employee.email,
-            'hire_date': test_employee.hire_date
-        }
-    }
 except PersonioError:
     can_authenticate = False
 skip_if_no_auth = pytest.mark.skipif(not can_authenticate, reason="Personio authentication failed")
 
-
+@lru_cache(maxsize=1)
+def get_test_employee():
+    return personio.get_employees()[0]
