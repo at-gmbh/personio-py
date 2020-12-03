053ccc64a3cd215beb85ef5536174a78151e07d7
Sebastian Straub <sebastian.straub@alexanderthamm.com>
2020-12-03 17:36:31 +0100

    cleanup

--- tests/apitest_shared.py
+++ tests/apitest_shared.py
@@ -1,7 +1,7 @@
 import os
-import pytest
-
 from functools import lru_cache
+
+import pytest
 
 from personio_py import Personio, PersonioError
 
@@ -10,19 +10,15 @@ CLIENT_ID = os.getenv('CLIENT_ID')
 CLIENT_SECRET = os.getenv('CLIENT_SECRET')
 personio = Personio(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
 
-
-@lru_cache(maxsize=1)
-def get_test_employee():
-    return personio.get_employees()[0]
-
-
 # deactivate all tests that rely on a specific personio instance
 try:
     personio.authenticate()
     can_authenticate = True
-    # This is used to ensure the test check for existing objects
 except PersonioError:
     can_authenticate = False
 skip_if_no_auth = pytest.mark.skipif(not can_authenticate, reason="Personio authentication failed")
 
 
+@lru_cache(maxsize=1)
+def get_test_employee():
+    return personio.get_employees()[0]
