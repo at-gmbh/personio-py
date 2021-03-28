79a1ff7d2dda6c2aed7f337668d1ee7eaaa1fe92
Sebastian Straub <sstraub@posteo.de>
2021-03-28 19:04:33 +0200

    add mock tests for search functions

--- tests/test_search.py
+++ tests/test_search.py
@@ -1,5 +1,8 @@
+import responses
+
 from personio_py.search import SearchIndex
 from tests.apitest_shared import personio, skip_if_no_auth
+from tests.test_mock_api import mock_employees, mock_personio
 
 
 @skip_if_no_auth
@@ -11,3 +14,29 @@ def test_search_index():
     assert index.valid
     assert index.index
     assert index.last_update > 0
+
+
+@responses.activate
+def test_mock_search():
+    # mock data & configure personio
+    mock_employees()
+    personio = mock_personio()
+    # search for employees
+    results = personio.search("Ada Alan")
+    assert len(results) == 2
+    # check search index state
+    index = personio.search_index
+    assert index.valid
+    assert index.index
+    assert index.last_update > 0
+
+
+@responses.activate
+def test_mock_search_first():
+    # mock data & configure personio
+    mock_employees()
+    personio = mock_personio()
+    # search for employees
+    result = personio.search_first("stallman")
+    assert result.first_name == "Richard"
+    assert result.last_name == "Stallman"
