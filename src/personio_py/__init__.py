4176929daebb33edd143bb89f4b6e859cb441e36
Sebastian Straub <sebastian.straub@alexanderthamm.com>
2023-07-28 16:04:48 +0200

    reset import order in __init__.py and add config to pyproject.toml

--- src/personio_py/__init__.py
+++ src/personio_py/__init__.py
@@ -7,11 +7,33 @@ a lightweight Personio API client
 __title__ = "personio-py"
 __copyright__ = "© 2020-2023 Alexander Thamm GmbH"
 
-from .client import Personio
-from .errors import MissingCredentialsError, PersonioApiError, PersonioError, UnsupportedMethodError
-from .models import Absence, AbsenceBalance, AbsenceEntitlement, AbsenceType, Attendance, \
-    BaseEmployee, CostCenter, CustomAttribute, Department, Employee, HolidayCalendar, Office, \
-    PersonioResource, PersonioResourceType, Project, ShortEmployee, Team, WorkSchedule, \
-    update_model
-from .search import SearchIndex
 from .version import __version__
+from .errors import (
+    MissingCredentialsError,
+    PersonioApiError,
+    PersonioError,
+    UnsupportedMethodError
+)
+from .models import (
+    update_model,
+    Absence,
+    AbsenceBalance,
+    AbsenceEntitlement,
+    AbsenceType,
+    Attendance,
+    BaseEmployee,
+    CostCenter,
+    CustomAttribute,
+    Department,
+    Employee,
+    HolidayCalendar,
+    Office,
+    PersonioResource,
+    PersonioResourceType,
+    ShortEmployee,
+    Team,
+    WorkSchedule,
+    Project
+)
+from .search import SearchIndex
+from .client import Personio
